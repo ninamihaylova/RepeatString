c45f40cf28cc961e7bc95a8d8734b1077a39e05b
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-07-01 20:07:10 +0300

    Initial commit

---
+++ RepeatString/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        
+        string text = Console.ReadLine();
+
+        
+        int repeatCount = int.Parse(Console.ReadLine());
+
+        string repeatedText = RepeatStringWithoutSpace(text, repeatCount);
+        Console.WriteLine(repeatedText);
+    }
+
+    static string RepeatStringWithoutSpace(string text, int repeatCount)
+    {
+        string result = "";
+        for (int i = 0; i < repeatCount; i++)
+        {
+            result += text;
+        }
+        return result;
+    }
+}
